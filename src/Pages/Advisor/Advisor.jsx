7141aa7c5a6dc916b33b219314e68f772c63e692
steve-cohen <43491271+steve-cohen@users.noreply.github.com>
2022-03-02 21:54:54 -0800

    CSS Hardcore to Grid

--- src/Pages/Advisor/Advisor.jsx
+++ src/Pages/Advisor/Advisor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 
 import Clients from './Clients/Clients'
 import Edit from './Portfolios/Edit/Edit'
@@ -18,13 +18,22 @@ const Advisor = ({ clients, email, portfolios, setPortfolios }) => {
 
 	return (
 		<div className='Advisor'>
-			<Navigation />
-			<div className='Body'>
-				<Routes>
-					<Route path='clients' element={<Clients clients={clients} portfolios={portfolios} />} />
-					<Route path='portfolios' element={<Portfolios portfolios={portfolios} />} />
-					<Route path='portfolios/edit' element={<Edit portfolios={portfolios} />} />
-				</Routes>
+			<div className='Metadata'>
+				<Link className='Logo' to='/'>
+					B
+				</Link>
+				<div className='Firm'>Hardy Capital</div>
+				<div className='Advisor'>Arynton Hardy</div>
+			</div>
+			<div className='Content'>
+				<Navigation />
+				<div className='Body'>
+					<Routes>
+						<Route path='clients' element={<Clients clients={clients} portfolios={portfolios} />} />
+						<Route path='portfolios' element={<Portfolios portfolios={portfolios} />} />
+						<Route path='portfolios/edit' element={<Edit portfolios={portfolios} />} />
+					</Routes>
+				</div>
 			</div>
 		</div>
 	)
