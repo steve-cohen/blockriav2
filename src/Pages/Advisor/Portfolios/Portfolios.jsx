2cd1592e080a57cb5d261e2a3703c6a402ec7e4d
steve-cohen <43491271+steve-cohen@users.noreply.github.com>
2022-04-07 09:35:54 -0700

    Portfolios Design

--- src/Pages/Advisor/Portfolios/Portfolios.jsx
+++ src/Pages/Advisor/Portfolios/Portfolios.jsx
@@ -2,67 +2,61 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Portfolios.css'
 
-function renderPortfolios({ allocations, portfolioId, portfolioName }) {
-	return (
-		<tr key={`Portfolio ${portfolioId}`}>
-			<td>{portfolioName}</td>
-			<td>
-				{allocations
-					.sort((a, b) => b.percent - a.percent)
-					.map(({ holding, percent }) => `${holding} ${percent}%`)
-					.join('\n')}
-			</td>
-			<td>
-				<Link to={`/advisor/portfolios/edit?portfolioId=${portfolioId}`}>Edit Portfolio</Link>
-			</td>
-			{/* <td>
-				<Link to={`/advisor/portfolios/assign?portfolioId=${portfolioId}`}>Assign Portfolio to Client(s)</Link>
-			</td> */}
-			<td></td>
-		</tr>
-	)
-}
-
 const Portfolios = ({ advisor, portfolios, setPortfolios }) => {
 	useEffect(async () => {
 		setPortfolios([])
 		fetch(`https://blockria.com/api/portfolios?advisorId=${advisor.idToken.payload.sub}`)
 			.then(response => response.json())
-			.then(newPortfolios => {
-				console.log(newPortfolios)
-				localStorage.setItem('portfolios', JSON.stringify(newPortfolios))
-				setPortfolios(newPortfolios)
-			})
+			.then(setPortfolios)
 			.catch(error => alert(error))
 	}, [])
 
+	function renderPortfolios({ allocations, portfolioId, portfolioName }) {
+		return (
+			<tr key={`Portfolio ${portfolioId}`}>
+				<td>{portfolioName}</td>
+				<td>
+					{allocations
+						.sort((a, b) => b.percent - a.percent)
+						.map(({ holding, percent }) => `${holding} ${percent}%`)
+						.join('\n')}
+				</td>
+				<td>
+					<Link to={`/advisor/portfolios/edit?portfolioId=${portfolioId}`}>Edit Portfolio</Link>
+				</td>
+				{/* <td>
+				<Link to={`/advisor/portfolios/assign?portfolioId=${portfolioId}`}>Assign Portfolio to Client(s)</Link>
+			</td> */}
+			</tr>
+		)
+	}
+
 	return (
-		<div className='Portfolios'>
-			<div className='Title'>Portfolios</div>
-			<div className='Options'>
-				<div></div>
-				<div>
-					<Link className='Option1' to='/advisor/portfolios/edit'>
+		<table>
+			<caption>
+				<div className='Flex'>
+					<div className='Title'>Portfolios</div>
+					<Link className='Button' to={`/advisor/portfolios/edit`}>
 						Create Portfolio
 					</Link>
 				</div>
-			</div>
-			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Allocations</th>
-						<th>Edit</th>
-						{/* <th>Assign</th> */}
-						<th />
-					</tr>
-				</thead>
-				<tbody>{portfolios.map(renderPortfolios)}</tbody>
-			</table>
-			<Link className='Option0' to='/advisor/portfolios/edit'>
-				+ Create Portfolio
-			</Link>
-		</div>
+			</caption>
+			<thead>
+				<tr>
+					<th>NAME</th>
+					<th>Allocations</th>
+					<th>EDIT</th>
+				</tr>
+			</thead>
+			<tbody>{portfolios.map(renderPortfolios)}</tbody>
+			<tfoot>
+				<tr>
+					<td colSpan={3}>
+						<Link to='/advisor/portfolios/edit'>+ Create Portfolio</Link>
+					</td>
+				</tr>
+			</tfoot>
+		</table>
 	)
 }
 
